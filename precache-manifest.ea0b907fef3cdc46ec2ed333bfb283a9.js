98e86e111baf0e87a4397cc5118089591d4459e2
Tuvshintur Batzorig <tuvshinturbatzorig@gmail.com>
2020-06-28 20:43:59 -0500

    changed folder structure for deploy on gh pages

---
+++ precache-manifest.ea0b907fef3cdc46ec2ed333bfb283a9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0212667907829c7545bf301188432a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db85a2498a847c0d596a",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "12c66aa0312f8c6b4f0d",
+    "url": "/static/css/main.8435c2c8.chunk.css"
+  },
+  {
+    "revision": "db85a2498a847c0d596a",
+    "url": "/static/js/2.a7f102ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a7f102ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12c66aa0312f8c6b4f0d",
+    "url": "/static/js/main.59dbad10.chunk.js"
+  },
+  {
+    "revision": "7ad8010695e2e3490ccf",
+    "url": "/static/js/runtime-main.aa750423.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
