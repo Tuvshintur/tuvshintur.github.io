c780ea1d3b0aef26f2e559681791b057ca728f10
Turuu <tuvshinturbatzorig@gmail.com>
2023-05-30 13:15:26 -0400

    release v3

---
+++ precache-manifest.ff05331fb28a593daa4b1a66100e3ae3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "353f3940d95f2e776600",
+    "url": "/static/css/main.226ad620.chunk.css"
+  },
+  {
+    "revision": "353f3940d95f2e776600",
+    "url": "/static/js/main.353f3940.chunk.js"
+  },
+  {
+    "revision": "dccf3f78d748d947d114",
+    "url": "/static/js/1.dccf3f78.chunk.js"
+  },
+  {
+    "revision": "5ed9c54db67de8e1ae0a",
+    "url": "/static/css/2.bfefa67c.chunk.css"
+  },
+  {
+    "revision": "5ed9c54db67de8e1ae0a",
+    "url": "/static/js/2.5ed9c54d.chunk.js"
+  },
+  {
+    "revision": "8772bf14478d7b7f1d72",
+    "url": "/static/js/runtime~main.8772bf14.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a00ab766a6699506f4b0108c11aa32f1",
+    "url": "/index.html"
+  }
+];
